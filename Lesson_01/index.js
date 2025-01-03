89f71836a30e32be6b098afd22f0ed9d53b3e4e0
VladPC <moonfoxy@icloud.com>
2025-01-03 18:42:45 +0300

    initial commit

---
+++ Lesson_01/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const productName = "Hot Dog";
+let productQuantity = "10";
+const productCategory = "Roasted_animals";
+let productPrice = "155";
+
+console.log('Имя товара:', productName)
+console.log('Общая сумма:', productQuantity * productPrice)
