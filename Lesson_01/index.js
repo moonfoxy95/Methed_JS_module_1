5b9b639141cc5af023b115f8271458fd3f4605d6
VladPC <moonfoxy@icloud.com>
2025-01-03 18:51:18 +0300

    added second product

--- Lesson_01/index.js
+++ Lesson_01/index.js
@@ -1,5 +1,6 @@
 'use strict';
 
+{
 const productName = "Hot Dog";
 let productQuantity = "10";
 const productCategory = "Roasted_animals";
@@ -7,3 +8,11 @@ let productPrice = "155";
 
 console.log('Имя товара:', productName)
 console.log('Общая сумма:', productQuantity * productPrice)
+}
+
+{
+const productName = "Parmezan";
+let productQuantity = "15";
+const productCategory = "Cheese";
+let productPrice = "10";
+}
